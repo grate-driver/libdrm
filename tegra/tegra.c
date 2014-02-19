a3926cf012e468b9eb9b15803d15c24f3c97f3c7
Thierry Reding <treding@nvidia.com>
2014-02-19 14:51:06 +0100

    tegra: Add channel, job, pushbuf and fence APIs
    These functions can be used to open channels to engines, manage job
    submissions, create push buffers to store command streams in and wait
    until jobs have been completed.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    ---
    Changes in v3:
    - make drm_tegra_fence_wait() a static inline to avoid overhead
    - prepare push buffers in sync helper
    - add missing sys/ioctl.h include
    - remove unused variables
    
    Changes in v2:
    - automatically allocate buffer objects as required by pushbuffers
      - pushbuffers can now have more than one associated buffer object
      - add drm_tegra_pushbuf_prepare() function

--- tegra/tegra.c
+++ tegra/tegra.c
@@ -52,6 +52,7 @@ static void drm_tegra_bo_free(struct drm_tegra_bo *bo)
 
 	drmIoctl(drm->fd, DRM_IOCTL_GEM_CLOSE, &args);
 
+	DRMLISTDEL(&bo->list);
 	free(bo);
 }
 
@@ -119,6 +120,7 @@ int drm_tegra_bo_new(struct drm_tegra_bo **bop, struct drm_tegra *drm,
 	if (!bo)
 		return -ENOMEM;
 
+	DRMINITLISTHEAD(&bo->list);
 	atomic_set(&bo->ref, 1);
 	bo->flags = flags;
 	bo->size = size;
