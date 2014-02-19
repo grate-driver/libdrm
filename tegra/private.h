ba51c8ccd2506f87f769f2e3a86d6420ec3d918f
Thierry Reding <treding@nvidia.com>
2014-02-19 14:51:06 +0100

    tegra: Add channel, job, pushbuf and fence APIs
    These functions can be used to open channels to engines, manage job
    submissions, create push buffers to store command streams in and wait
    until jobs have been completed.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    ---
    Changes in v3:
    - make drm_tegra_fence_wait() a static inline to avoid overhead
    - prepare push buffers in sync helper
    - add missing sys/ioctl.h include
    - remove unused variables
    
    Changes in v2:
    - automatically allocate buffer objects as required by pushbuffers
      - pushbuffers can now have more than one associated buffer object
      - add drm_tegra_pushbuf_prepare() function

--- tegra/private.h
+++ tegra/private.h
@@ -26,12 +26,30 @@
 #define __DRM_TEGRA_PRIVATE_H__ 1
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
+#include <libdrm_lists.h>
 #include <libdrm_macros.h>
 #include <xf86atomic.h>
 
+#include "tegra_drm.h"
 #include "tegra.h"
+
+#define container_of(ptr, type, member) ({				\
+		const typeof(((type *)0)->member) *__mptr = (ptr);	\
+		(type *)((char *)__mptr - offsetof(type, member));	\
+	})
+
+#define align(offset, align) \
+	(((offset) + (align) - 1) & ~((align) - 1))
+
+enum host1x_class {
+	HOST1X_CLASS_HOST1X = 0x01,
+	HOST1X_CLASS_GR2D = 0x51,
+	HOST1X_CLASS_GR2D_SB = 0x52,
+	HOST1X_CLASS_GR3D = 0x60,
+};
 
 struct drm_tegra {
 	bool close;
@@ -40,6 +58,7 @@ struct drm_tegra {
 
 struct drm_tegra_bo {
 	struct drm_tegra *drm;
+	drmMMListHead list;
 	uint32_t handle;
 	uint32_t offset;
 	uint32_t flags;
@@ -47,5 +66,58 @@ struct drm_tegra_bo {
 	atomic_t ref;
 	void *map;
 };
+
+struct drm_tegra_channel {
+	struct drm_tegra *drm;
+	enum host1x_class class;
+	uint64_t context;
+	uint32_t syncpt;
+};
+
+struct drm_tegra_fence {
+	struct drm_tegra *drm;
+	uint32_t syncpt;
+	uint32_t value;
+};
+
+struct drm_tegra_pushbuf_private {
+	struct drm_tegra_pushbuf base;
+	struct drm_tegra_job *job;
+	drmMMListHead list;
+	drmMMListHead bos;
+
+	struct drm_tegra_bo *bo;
+	uint32_t *start;
+	uint32_t *end;
+};
+
+static inline struct drm_tegra_pushbuf_private *
+drm_tegra_pushbuf(struct drm_tegra_pushbuf *pb)
+{
+	return container_of(pb, struct drm_tegra_pushbuf_private, base);
+}
+
+int drm_tegra_pushbuf_queue(struct drm_tegra_pushbuf_private *pushbuf);
+
+struct drm_tegra_job {
+	struct drm_tegra_channel *channel;
+
+	unsigned int increments;
+	uint32_t syncpt;
+
+	struct drm_tegra_reloc *relocs;
+	unsigned int num_relocs;
+
+	struct drm_tegra_cmdbuf *cmdbufs;
+	unsigned int num_cmdbufs;
+
+	struct drm_tegra_pushbuf_private *pushbuf;
+	drmMMListHead pushbufs;
+};
+
+int drm_tegra_job_add_reloc(struct drm_tegra_job *job,
+			    const struct drm_tegra_reloc *reloc);
+int drm_tegra_job_add_cmdbuf(struct drm_tegra_job *job,
+			     const struct drm_tegra_cmdbuf *cmdbuf);
 
 #endif /* __DRM_TEGRA_PRIVATE_H__ */
