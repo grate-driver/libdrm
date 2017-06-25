1ca834e399eed4e50efd7e9624528fba5b3c943d
Dmitry Osipenko <digetx@gmail.com>
2017-06-25 14:32:17 +0300

    tegra: Restore correct error number passing from drmIoctl
    drmIoctl() passes a raw error value from ioctl(), we should return a errno.
    That behaviour was changed by commit a9c0405d (Implement BO handle and name
    hashtable lookup), let's restore it.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- tegra/tegra.c
+++ tegra/tegra.c
@@ -73,6 +73,8 @@ drm_private int drm_tegra_bo_free(struct drm_tegra_bo *bo)
 	args.handle = bo->handle;
 
 	err = drmIoctl(drm->fd, DRM_IOCTL_GEM_CLOSE, &args);
+	if (err < 0)
+		err = -errno;
 
 	free(bo);
 
@@ -480,6 +482,7 @@ int drm_tegra_bo_from_name(struct drm_tegra_bo **bop, struct drm_tegra *drm,
 
 	err = drmIoctl(drm->fd, DRM_IOCTL_GEM_OPEN, &args);
 	if (err < 0) {
+		err = -errno;
 		free(bo);
 		bo = NULL;
 		goto unlock;
