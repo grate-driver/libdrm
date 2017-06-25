d01ffd0100648d6aabaec03cdac2de79a74fd406
Dmitry Osipenko <digetx@gmail.com>
2017-06-25 16:27:49 +0300

    tegra: Use drmCommandWriteRead for Tegra IOCTL's
    Let's be consistent by making use of drmCommandWriteRead for all of
    Tegra IOCTL's.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- tegra/tegra.c
+++ tegra/tegra.c
@@ -390,7 +390,6 @@ drm_public int drm_tegra_bo_set_tiling(struct drm_tegra_bo *bo,
 			    const struct drm_tegra_bo_tiling *tiling)
 {
 	struct drm_tegra_gem_set_tiling args;
-	int err;
 
 	if (!bo || !tiling)
 		return -EINVAL;
@@ -400,12 +399,8 @@ drm_public int drm_tegra_bo_set_tiling(struct drm_tegra_bo *bo,
 	args.mode = tiling->mode;
 	args.value = tiling->value;
 
-	err = drmCommandWriteRead(bo->drm->fd, DRM_TEGRA_GEM_SET_TILING, &args,
-				  sizeof(args));
-	if (err < 0)
-		return err;
-
-	return 0;
+	return drmCommandWriteRead(bo->drm->fd, DRM_TEGRA_GEM_SET_TILING,
+				   &args, sizeof(args));
 }
 
 int drm_tegra_bo_get_name(struct drm_tegra_bo *bo, uint32_t *name)
