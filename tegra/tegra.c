782c3b5a96b8ebc76bf1e28f44c85edc80331df5
Erik Faye-Lund <kusmabite@gmail.com>
2017-06-26 18:55:12 +0000

    tegra: guard valgrind macros with HAVE_VALGRIND
    This fixes a compilation-error when valgrind-headers are not
    available.
    
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

--- tegra/tegra.c
+++ tegra/tegra.c
@@ -285,6 +285,7 @@ drm_public int drm_tegra_bo_map(struct drm_tegra_bo *bo, void **ptr)
 		bo->mmap_ref++;
 	}
 
+#ifdef HAVE_VALGRIND
 	if (RUNNING_ON_VALGRIND) {
 		/*
 		 * BO is mapped on creation under valgrind, hence
@@ -300,6 +301,7 @@ drm_public int drm_tegra_bo_map(struct drm_tegra_bo *bo, void **ptr)
 		if (bo->mmap_ref == 2)
 			VALGRIND_MAKE_MEM_DEFINED(bo->map, bo->size);
 	}
+#endif
 
 unlock:
 	pthread_mutex_unlock(&table_lock);
@@ -322,12 +324,14 @@ drm_public int drm_tegra_bo_unmap(struct drm_tegra_bo *bo)
 	if (!bo->map)
 		goto unlock;
 
+#ifdef HAVE_VALGRIND
 	/*
 	 * mmap_ref is bumped by one under valgrind, hence disable
 	 * access on 2
 	 */
 	if (RUNNING_ON_VALGRIND && bo->mmap_ref == 2)
 		VALGRIND_MAKE_MEM_NOACCESS(bo->map, bo->size);
+#endif
 
 	if (--bo->mmap_ref > 0)
 		goto unlock;
