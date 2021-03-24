af871ec1a6f017b782943a11fb1c06b18c41bfe3
Lang Yu <Lang.Yu@amd.com>
2021-03-24 12:18:41 +0800

    drm/tests/amdgpu: fix Metadata test failed issue
    The unit of size_metadata is one byte not four bytes.
    Enable Metadata test.
    
    Signed-off-by: Lang Yu <Lang.Yu@amd.com>
    Signed-off-by: Marek Olšák <marek.olsak@amd.com>

--- tests/amdgpu/amdgpu_test.c
+++ tests/amdgpu/amdgpu_test.c
@@ -496,9 +496,6 @@ static void amdgpu_disable_suites()
 				"gfx ring slow bad draw test (set amdgpu.lockup_timeout=50)", CU_FALSE))
 			fprintf(stderr, "test deactivation failed - %s\n", CU_get_error_msg());
 
-	if (amdgpu_set_test_active(BO_TESTS_STR, "Metadata", CU_FALSE))
-		fprintf(stderr, "test deactivation failed - %s\n", CU_get_error_msg());
-
 	if (amdgpu_set_test_active(BASIC_TESTS_STR, "bo eviction Test", CU_FALSE))
 		fprintf(stderr, "test deactivation failed - %s\n", CU_get_error_msg());
 
